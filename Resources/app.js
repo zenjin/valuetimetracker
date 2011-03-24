70845ca80f8f5a3c9d7d1154fdf6014b9df49898
zenjin <e.med@gmx.de>
2011-03-24 18:48:29 +0100

    runs on android now, fuckergit statusgit status11

--- Resources/app.js
+++ Resources/app.js
@@ -13,9 +13,32 @@ var RUNTESTS = true;
 // -------------------------------------------------------------------------- //
 
 if (RUNTESTS) {
-	//var TEST_SUITE = 'core'; // Unit Tests
+	var TEST_SUITE = 'core'; // Unit Tests
 	//var TEST_SUITE = ''; // Graphical Test
-	Ti.include('/tests/run.js');
+	var win = Ti.UI.createWindow();
+	//win.add(Ti.UI.createImageView({image:'helium.png', height:'auto', width:'auto'}));
+	win.add(Ti.UI.createLabel({
+		text:'Unit tests in progress - check the console for details.',
+		color:'#787878',
+		bottom:20,
+		right:10,
+		left:10,
+		font:{fontSize:12},
+		height:'auto',
+		textAlign:'center'
+	}));
+	win.open();
+
+	//Include tests
+	Ti.include(
+		'/helium.js',
+		'/tests/test.js',
+		'/tests/core.js',
+		'/tests/ui.js'
+	);
+	var suite = (typeof(TEST_SUITE)!='undefined') ? TEST_SUITE : '';
+	he.test.run(suite);
+
 } else {
 	Ti.include('/app/app.js');
 }
